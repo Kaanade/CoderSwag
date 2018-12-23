697f540bd96e7ebe42b45f9c1d17f3a7c76f9664
matth <matthieu.bertho@orange.fr>
2018-12-23 12:29:21 +0100

    Simple list & listCategory layout

---
+++ app/src/main/java/com/apps/elkaan/coderswag/Controller/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.apps.elkaan.coderswag.Controller
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.apps.elkaan.coderswag.Model.Category
+import com.apps.elkaan.coderswag.R
+import com.apps.elkaan.coderswag.Services.DataService
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var adapter: ArrayAdapter<Category>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        adapter = ArrayAdapter(this,
+            android.R.layout.simple_list_item_1,
+            DataService.categories)
+
+        categoryListView.adapter = adapter
+
+    }
+}
---
+++ app/src/main/java/com/apps/elkaan/coderswag/Model/Product.kt
@@ -0,0 +1,3 @@
+package com.apps.elkaan.coderswag.Model
+
+class Product(val title:String, val price:String, val image:String)
